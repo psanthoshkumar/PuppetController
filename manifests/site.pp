049289b51d405570e1edea1fb62839ac07e9c243
sam <psanthoshkumar08@gmail.com>
2019-01-06 17:32:59 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,12 @@
 node default {
 	file {'/root/README':
 	ensure	=>	file,
-	content	=>	'this is testing file'
+	content	=>	'this is testing file\n',
+	owner	=> 'root',
+
+	}
+	file {'/root/README':
+	owner	=>	'root',
 
 	}
 }
